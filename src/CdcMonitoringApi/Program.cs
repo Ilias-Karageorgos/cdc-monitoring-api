aac374c9fb678ac5c0723b2b1a35a258b8ede21d
IliasKar <karageorgosiliass@gmail.com>
2026-06-20 18:00:13 +0300

    Configure Serilog logging via appsettings.json with daily rotation

--- src/CdcMonitoringApi/Program.cs
+++ src/CdcMonitoringApi/Program.cs
@@ -4,27 +4,33 @@ using Microsoft.EntityFrameworkCore;
 using CdcMonitoringApi.IRepositories;
 using CdcMonitoringApi.Repositories;
 using CdcMonitoringApi.Services;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 builder.Services.AddScoped<IMetricRepository, MetricRepository>();
 builder.Services.AddScoped<IErrorRepository, ErrorRepository>();
 builder.Services.AddHostedService<MetricGeneratorService>();
 
+builder.Logging.ClearProviders();
+builder.Services.AddSerilog(config =>
+        config.ReadFrom.Configuration(builder.Configuration));
+
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
